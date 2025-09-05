6ade864eaf6b219963b2f99157e40fa06b62f2b6
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2025-09-05 16:37:44 -0700

    pems-bay added 1-18

--- datasets/pemsbay_traffic/process_pemsbay.py
+++ datasets/pemsbay_traffic/process_pemsbay.py
@@ -45,15 +45,17 @@ data = pd.DataFrame({
 print(f"data epoch before: {data['epoch']}")
 data = data.loc[data.index.repeat(coords_len)].reset_index(drop=True)
 
-time_len = df_date['datetime'].shape[0] // 20
+time_len = df_date['datetime'].shape[0]
 print(f"time len: {time_len}, coords len: {coords_len}")
 data_epoch = data['epoch'].values[:int(len(data['epoch'].values)/20)]
 print(f"data epoch: {data_epoch.shape}")
 data_speed = speed.reshape(-1)[:int(len(speed.reshape(-1))/20)]
 print(f"data speed: {data_speed.shape}")
 data_longitude = np.tile(df_locs['longitude'].values, time_len)
+data_longitude = data_longitude[:data_longitude//20]
 print(f"data longitude: {data_longitude.shape}")
 data_latitude = np.tile(df_locs['latitude'].values, time_len)
+data_latitude = data_latitude[:data_latitude//20]
 print(f"data latitude: {data_latitude.shape}")
 dict_values = {
     'datetime': data_epoch,
